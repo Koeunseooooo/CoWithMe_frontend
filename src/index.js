12d493536476339e28fe3a669b966fda26bf6134
hyosin-Jang <cdnnnl@gmail.com>
2022-06-21 16:54:24 +0900

    initial setting using Create React App

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { theme } from './styles/theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <ThemeProvider theme={theme} >
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
