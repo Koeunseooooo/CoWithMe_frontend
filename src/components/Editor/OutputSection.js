ddb366c465665a780f325cd5236ec3e0a217b546
hyosin-Jang <cdnnnl@gmail.com>
2022-06-23 03:13:32 +0900

    Style: 레이아웃 수정

--- src/components/Editor/OutputSection.js
+++ src/components/Editor/OutputSection.js
@@ -28,8 +28,8 @@ const OutputSection = () => {
                 {result !== "" && <>{result}</>}
             </div>
             <div className="button-wrapper">
-                <button onClick={submit}>제출하기</button>
-                <button>다음 문제 풀기</button>
+                <Button color="grey" onClick={submit}>제출하기</Button>
+                <Button color="green">다음 문제 풀기</Button>
             </div>
         </OutputWrapper>
     );
@@ -38,16 +38,45 @@ const OutputSection = () => {
 export default OutputSection;
 
 const OutputWrapper = styled.div`
-
+padding: 30px;
 margin-top: 40px;
 display: flex;
 flex-direction: column;
-background-color: pink;
+background-color: ${theme.color.black4};
 .console{
     padding: 30px;
+    margin-top: 36px;
+    height: 68px;
+    font-size: 18px;
+    
 }
-.button-wraper{
+.button-wrapper{
+    ${flexCenter}
+    gap: 40px;
 
+}
+.output-title{
+    font-family: 'AppleSDGothicNeoB00';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 33px;
+    letter-spacing: 0.25px;
+    color: ${theme.color.green}
 }
 
 `;
+
+const Button = styled.button`
+width: 165px;
+height: 48px;
+border-radius: 100px;
+color: ${(props) => props.color === 'grey' ? theme.color.white : theme.color.black};
+background-color: ${(props) => props.color === 'grey' ? theme.color.grey : theme.color.green};
+font-family: 'AppleSDGothicNeoB00';
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+letter-spacing: 0.25px;
+
+`
