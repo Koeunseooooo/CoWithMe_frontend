bff56930e7b5407cb000970f9268aebe983a3276
thestarkor <sk_and_mc@kaist.ac.kr>
2022-06-22 17:07:10 +0900

    [feat] 가입, 로그인, 유저 로딩 api 예시

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
 import { useCookies } from "react-cookie"
 
 const Home = () => {
   const [, setCookie, removeCookie] = useCookies(["Authorization"]);
 
-  const loginTest = () => {
-    setCookie("Authorization", "abcd", {
+  const [email, setEmail] = useState("");
+
+  const join = async () => {
+    // const resp = await axios.post('/auth/join', {})
+
+    setCookie("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJoQGguY29tIiwiaWF0IjoxNjU1ODg0NTg1LCJleHAiOjE2NTU5NzA5ODV9.vgHvGEy400xVeMemCdLg9PESL75KJY6_fyMzfOsy46A", {
       path: "/",
       maxAge: 24 * 60 * 60,
     })
+    window.location.href = "/";
   }
 
-  const logoutTest = () => {
+  const login = async () => {
+    // const resp = await axios.post('/auth/join', {})
+
+    setCookie("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJoQGguY29tIiwiaWF0IjoxNjU1ODg0NTg1LCJleHAiOjE2NTU5NzA5ODV9.vgHvGEy400xVeMemCdLg9PESL75KJY6_fyMzfOsy46A", {
+      path: "/",
+      maxAge: 24 * 60 * 60,
+    })
+    window.location.href = "/";
+  }
+
+  const logout = () => {
     removeCookie("Authorization");
+    window.location.href = "/";
+  }
+
+  const loadUser = () => {
+    axios.get('/users/me')
+      .then(res => {
+        console.log(res.data);
+        setEmail(res.data?.email)
+      })
   }
 
   return (
     <>
-      <button onClick={loginTest}>Login</button>
-      <button onClick={logoutTest}>LogOut</button>
+      <h3>{email}</h3>
+      <button onClick={join}>Join</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>LogOut</button>
+      <button onClick={loadUser}>Load</button>
     </>
   )
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,15 @@
-import axios from "axios";
-import { Suspense, useEffect } from "react"
+import { Suspense } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 import Home from "../pages/Home"
-import JoinTest from "../pages/JoinTest";
 import MyPage from "../pages/MyPage";
 
 const Router = () => {
-
-  // TODO: user 정보 호출
-  useEffect(() => {
-    axios.get('/users/me')
-      .then(res => {
-        console.log(res.data);
-      })
-  }, [])
-
   return (
     <Suspense>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/me" element={<MyPage />} />
-        <Route exact path="/join" element={<JoinTest />} />
 
         <Route path="*" element={<Navigate replace to="/" />} /> 
       </Routes>
