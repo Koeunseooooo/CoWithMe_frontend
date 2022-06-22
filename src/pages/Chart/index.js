ae7b4c43026521e56a259efd6df98037f73bd744
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-06-22 18:04:32 +0900

    [chore] apexchart 라이브러리 설치

---
+++ src/pages/Chart/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useState } from "react";
+import { useCookies } from "react-cookie"
+
+const Chart = () => {
+  const [, setCookie, removeCookie] = useCookies(["Authorization"]);
+
+
+
+
+  return (
+    <>
+
+      
+    </>
+  )
+}
+
+export default Chart;
