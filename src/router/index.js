41b324a8bd5637060478d45c3093764136ce0e0c
hyosin-Jang <cdnnnl@gmail.com>
2022-06-23 07:03:57 +0900

    Feat: 랜딩 페이지 라우팅

--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,22 @@ import Home from "../pages/Home"
 import MyPage from "../pages/MyPage";
 import Solve from "../pages/Solve";
 import Chart from "../pages/Chart";
+import Landing from "../pages/Landing";
 
 import Header from "../components/Header"
 
 const Router = () => {
   return (
-    
+
     <Suspense>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/me" element={<MyPage />} />
         <Route exact path="/solve/:id" element={<Solve />} />
         <Route exact path="/chart" element={<Chart />} />
+        <Route exact path="/landing" element={<Landing />} />
 
-        <Route path="*" element={<Navigate replace to="/" />} /> 
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </Suspense>
   )
