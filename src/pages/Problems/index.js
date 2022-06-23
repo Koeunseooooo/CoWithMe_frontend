c8a551000acd9969d39cbedfea3bb4fd02a15189
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-06-24 01:12:03 +0900

    Feat: 체크박스 css 적용 (중복 체크 핸들링 안함)

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -65,14 +65,57 @@ const Problems = ({ history }) => {
     <>
       <SubHeader headText={"맞춤 유형 문제"} />
       <Header isTest />
-      {/* <div className="section-name">
-                  내 코딩 영역 차트
-                </div> */}
         <Wrapper>
+        <span className="description">Problem and solve</span>
+        <span className="subheading"> | Basic Test</span>
           <div className="main-wrapper">
             <ContentWrapper>
-              <div className="each-wrapper">
+              <div className="each-wrapper-left">
                 <div className="section">
+                  <div className="searchbar">
+                    <img className="sub_icon" src={require('../../assets/Sub Icon/Serch.svg').default} alt="" />
+                    <div className="search_content"> | 이름 검색 </div>
+                  </div>
+                  <hr className="line" />
+
+                  
+                  <div className="check-block">
+                    <input type="checkbox" id="check1"/>
+                    <label for="check1"></label>
+                    <div className="content">DFS/BFS</div>
+                  </div>
+
+                  <div className="check-block">
+                    <input type="checkbox" id="check2"/>
+                    <label for="check2"></label>
+                    <div className="content">그리디/탐색</div>
+                  </div>
+
+                  <div className="check-block">
+                    <input type="checkbox" id="check3"/>
+                    <label for="check3"></label>
+                    <div className="content">DP</div>
+                  </div>
+
+                  <div className="check-block">
+                    <input type="checkbox" id="check4"/>
+                    <label for="check4"></label>
+                    <div className="content">문자열</div>
+                  </div>
+
+                  <div className="check-block">
+                    <input type="checkbox" id="check5"/>
+                    <label for="check5"></label>
+                    <div className="content">구현</div>
+                  </div>
+
+                  <div className="check-block">
+                    <input type="checkbox" id="check6"/>
+                    <label for="check6"></label>
+                    <div className="content">기타</div>
+                  </div>
+
+
                 </div>
               </div>
               <div className="each-wrapper">
@@ -117,11 +160,29 @@ const RightWrapper = styled.div`
 const Wrapper = styled.div`
   margin-left: 143px;
   padding: 20px;
+  .description{
+    margin-left:38px;
+    color: ${theme.color.green};
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 33px;
+    letter-spacing: 0.25px;
+  }
+
+  .subheading{
+    color: #ADADAC;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 20px;
+    letter-spacing: 0.25px;
+    
+  }
+    
 
 `;
 const ContentWrapper = styled.div`
   display: grid;
-  margin-top:120px;
+  margin-top:6px;
   padding: 30px;
   width: 100%;
   min-height: 100vh;
@@ -130,7 +191,7 @@ const ContentWrapper = styled.div`
   background-color: ${theme.color.black3};
   box-shadow: 0px 0px 15px rgba(255, 255, 255, 0.16);
   border-radius: 40px 40px 0px 0px;
-  grid-template-columns: 250px 400px 400px;
+  grid-template-columns: 300px 400px 400px;
 
   .main-wrapper{
     display: flex;
@@ -142,6 +203,15 @@ const ContentWrapper = styled.div`
   //   background-color:red;
   // }
 
+  .each-wrapper-left{
+    // background-color:red;
+    border-right-style: solid;
+    border-right-width: 1px;
+  }
+
+  .sub_icon{
+    margin-left:5px;
+  }
   .section-name{
     font-family: neoEB;
     font-style: normal;
@@ -158,6 +228,137 @@ const ContentWrapper = styled.div`
     flex-direction:column;
   }
 
+  .searchbar{
+    margin-top:20px;
+    color:#CBCBCA;
+    display:flex;
+  }
+
+  .search_content{
+    margin-left:15px;
+    font-family: 'AppleSDGothicNeoB00';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    /* identical to box height */
+    letter-spacing: 0.25px;
+    color: #FFFFFF;
+    // margin-left:30px;
+  }
+
+}
+
+.line{
+  margin-top:13px;
+  width:270px;
+  margin-right:25px;
+  color:#CBCBCA;
+  margin-bottom:25px;
+}
+
+input[type="checkbox"]{
+  display: none;
+}
+input[type="checkbox"] + label{
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border: 1px solid #D9D9D9;
+  border-radius: 3px;
+  position: relative;
+}
+
+input[id="check1"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+input[id="check2"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+input[id="check3"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+input[id="check4"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+input[id="check5"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+input[id="check6"]:checked + label::after{
+  content:'';
+  background:#2DDE54;
+  font-size: 15px;
+  width: 15px;
+  height: 15px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  top:0;
+}
+
+
+.content{
+  margin-left:15px;
+    font-family: 'AppleSDGothicNeoB00';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 20px;
+    /* identical to box height */
+    letter-spacing: 0.25px;
+    color: #FFFFFF;
+    // margin-left:30px;
+}
   
-  
+
+.check-block{
+  display:flex;
+  margin-top:25px;
+  margin-bottom:25px;
+}
 `;
