0d4e03a2d68088fc7ce587a03e6a12cdf7f03a54
hyosin-Jang <cdnnnl@gmail.com>
2022-06-24 03:51:35 +0900

    Feat: 회원가입 유효성 체크

--- src/pages/Landing/Signup.js
+++ src/pages/Landing/Signup.js
@@ -10,22 +10,52 @@ const Signuppage = () => {
     const [email, setEmail] = useState();
     const [nickname, setNickname] = useState();
     const [password, setPassword] = useState();
+    const [emailError, setEmailError] = useState();
+    const [pwError, setpwError] = useState();
+    const [nicknameError, setNicknameError] = useState();
+
     const navigate = useNavigate();
     const handleEmailChange = (e) => setEmail(e.target.value);
     const handlePWChange = (e) => setPassword(e.target.value);
     const handleNicknameChange = (e) => setNickname(e.target.value);
 
+    const passwordValidCheck = (password) => {
+        const specialLetter = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+        const result = password.length >= 8 && specialLetter >= 1;
+        result ? setpwError() : setpwError("올바른 비밀번호를 입력해주세요.");
+        return result;
+    }
 
-    // TODO: valid 체크
-    const handleSubmit = () => {
-        //axios.post('/user/signup', { email: email, password: password, nickname: nickname }).then((res) => {
-        //    console.log(res);
-        //    if (res.access_token) { // 토큰 받으면
-        //        localStorage.setItem('login-token'); // 로컬 스토리지에 저장
-        //        window.location.href = '/landing/baekjoon'; // 백준 페이지로 이동
-        //    }
-        //})
-        navigate(`/`);
+    const emailValidCheck = (email) => {
+        const emailRegex =
+            /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+        let result = emailRegex.test(email);
+        result ? setEmailError() : setEmailError("올바른 이메일을 입력해주세요.");
+        return result;
+    }
+
+    const nickNameValidCheck = (nickname) => {
+        const result = nickname.length > 2;
+        result ? setNicknameError() : setNicknameError("두 글자 이상 입력해주세요");
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let emailResult = emailValidCheck(email);
+        let passwordResult = passwordValidCheck(password);
+        let nicknameResult = nickNameValidCheck(nickname);
+        if (emailResult && passwordResult && nicknameResult) {
+
+            //axios.post('/user/signup', { email: email, password: password, nickname: nickname }).then((res) => {
+            //    console.log(res);
+            //    if (res.access_token) { // 토큰 받으면
+            //        localStorage.setItem('login-token'); // 로컬 스토리지에 저장
+            //        window.location.href = '/landing/baekjoon'; // 백준 페이지로 이동
+            //    }
+            //})
+        }
+        //navigate(`/`);
     }
 
     return (
@@ -36,8 +66,11 @@ const Signuppage = () => {
             <form onSubmit={handleSubmit}>
                 <div className="content-area-2">
                     <Input placeholder="이메일을 입력해주세요!" name="email" onChange={handleEmailChange} />
+                    <ErrorMessage>{emailError}</ErrorMessage>
                     <Input placeholder="사용할 닉네임을 입력해주세요!" name="nickname" onChange={handlePWChange} />
+                    <ErrorMessage>{nicknameError}</ErrorMessage>
                     <Input placeholder="비밀번호를 입력해주세요!" name="password" type="password" onChange={handleNicknameChange} />
+                    <ErrorMessage>{pwError}</ErrorMessage>
                     <LoginButton>회원가입 완료</LoginButton>
                 </div>
             </form>
@@ -46,6 +79,14 @@ const Signuppage = () => {
 }
 
 export default Signuppage;
+
+const ErrorMessage = styled.div`
+    color: red;
+    width: 601px;
+    height: 10px;
+    font-family: 'AppleSDGothicNeoB00';
+    font-size: 13px;
+`;
 
 const LaunchContainer = styled.div`
     width: 100%;
@@ -67,7 +108,6 @@ const LaunchContainer = styled.div`
 
     .content-area-2{
       ${flexCenter}
-      margin-top: 20px;
       flex-direction: column;
       gap: 8px;
       .other-method{
@@ -94,6 +134,7 @@ ${flexCenter}
 background: #F4F4F4;
 border-radius: 10px;
 border: none;
+margin-top: 15px;
 font-family: 'AppleSDGothicNeoEB00';
 font-style: normal;
 font-weight: 400;
@@ -109,11 +150,12 @@ color: black;
 
 const Input = styled.input`
 width: 601px;
-margin-bottom: 20px;
+margin-bottom: 5px;
+
 border:none; 
 background-color:  ${theme.color.green};
 border-bottom: 1px solid #000;
-padding: 10px 0px;
+padding: 15px 0px;
 font-weight: 400;
 font-family: 'AppleSDGothicNeoB00';
 font-size: 18px;
